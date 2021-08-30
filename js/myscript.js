caefcd8f121b05c87e0a74127dd82ac83f60a5be
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-08-30 14:38:38 +0200

    inzializzazione pagina ed inizio js

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+let nomePanino = document.getElementById("burger-name");
+
+let bottone = document.getElementById("calculate-button");
+
+bottone.addEventListener("click" ,
+function (){
+    if (nomePanino.value.lenght === 0){
+        alert("Perfavore inserire nome panino!")
+    }
+
+
+
+
+
+
+
+
+
+});

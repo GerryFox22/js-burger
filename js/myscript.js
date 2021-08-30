6be9abc848d5caea55092520a44f49f8c1ad207b
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-08-30 15:20:09 +0200

    primi avanzamenti js

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,48 @@
-let nomePanino = document.getElementById("burger-name");
+let listaCoupon = ["capodanno", "natale", "pasqua", "compleanno"]
 
+let nomePanino = document.getElementById("burger-name");
+let prezzoFinalePanino = 0;
 let bottone = document.getElementById("calculate-button");
+
+
 
 bottone.addEventListener("click" ,
 function (){
-    if (nomePanino.value.lenght === 0){
+    if (nomePanino.value.length === 0){
         alert("Perfavore inserire nome panino!")
+    } else {
+        let prezzoPartenzaPanino = 15;
+        let ingredienti = document.getElementsByClassName("ingredients-check");
+
+        for (let i = 0; i < ingredienti.length; i++){
+
+            let ingredienteSingolo = ingredienti [i];
+            
+            if (ingredienteSingolo.checked == true){
+
+                prezzoPartenzaPanino += 2 ;
+
+            }
+            
+        }
+        
+        console.log(prezzoPartenzaPanino);
+
+        prezzoFinalePanino += prezzoPartenzaPanino;
     }
 
-
-
-
-
-
-
-
-
 });
+console.log(prezzoFinalePanino);
+  
+let coupon = document.getElementById("insert-coupon");
+console.log(coupon.value);
+
+// for (let y = 0; y < listaCoupon.length; y++){
+
+//     if ( coupon === listaCoupon[y]){
+//         prezzoPartenzaPanino = prezzoPartenzaPanino * 0.8;
+//     }
+
+// }
+
+// console.log(prezzoPartenzaPanino);
